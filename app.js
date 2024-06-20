178617d8d1a308133aeead3e7e6649679fca93d4
CastPJ <JPajak.dev@gmail.com>
2024-06-20 11:43:16 +0200

    Added better functionality

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-/*
-<input
-                  class="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                /> 
+const form = document.getElementById("form-check");
 
+function addItem(textValue, idValue) {
+  // Creating checkbox
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("value", "");
+  checkbox.setAttribute("id", idValue);
+  checkbox.classList.add("form-check-input");
 
-                <label class="form-check-label" for="flexCheckDefault">
-    Default checkbox
-  </label>
-*/
+  // Creating label for checkbox
+  const label = document.createElement("label");
+  label.setAttribute("for", idValue);
+  label.classList.add("form-check-label");
+  label.textContent = textValue;
 
-const ul = document.getElementById("list-wrapper");
-// Creating checkbox
-const checkbox = document.createElement("input");
-checkbox.setAttribute("type", "checkbox");
-checkbox.setAttribute("value", "");
-checkbox.setAttribute("id", "flexCheckDefault");
-checkbox.classList.add("form-check-input");
-// Creating label for checkbox
-const label = document.createElement("label");
-label.setAttribute("for", "flexCheckDefault");
-label.classList.add("form-check-label");
-label.textContent = "Test content";
+  // Creating a wrapper div for better structure
+  const div = document.createElement("div");
+  div.classList.add("form-check");
 
-ul.appendChild(checkbox);
-ul.appendChild(label);
+  // Appending checkbox and label to the wrapper div
+  div.appendChild(checkbox);
+  div.appendChild(label);
+
+  // Appending the wrapper div to the form
+  form.appendChild(div);
+}
+
+addItem("test", "1");
+addItem("test2", "2");
+addItem("test3", "3");
